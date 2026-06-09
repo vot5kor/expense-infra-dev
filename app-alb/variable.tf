b4a63a0c75a0f2964106a8ab18e22b573c681b3b
vot5kor <vot5kor@gmail.com>
2026-06-09 18:45:08 +0530

    bastian

--- app-alb/variable.tf
+++ app-alb/variable.tf
@@ -16,9 +16,9 @@ variable "common_tags" {
 
 
 variable "zone_id" {
-    default = "Z021448929NMXW4P65UQE"
+    default = "Z02156493RNC9JYFJZDOR"
 }
 
 variable "domain_name" {
-    default = "daws82s.online"
+    default = "zenzee.fun"
 }
