18069041b5511d9f6faa7194468e78ca041b6477
vot5kor <vot5kor@gmail.com>
2026-08-20 14:52:56 +0530

    expense ansible roles

--- 60-backend/data.tf
+++ 60-backend/data.tf
@@ -1,20 +1,22 @@
+
 data "aws_ami" "joindevops" {
-    most_recent      = true
-    owners           = ["973714476881"]
-    filter {
-        name   = "name"
-        values = ["RHEL-9-DevOps-Practice"]
-    }
+  most_recent      = true
+  owners           = ["973714476881"]
 
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 data "aws_ssm_parameter" "backend_sg_id" {
