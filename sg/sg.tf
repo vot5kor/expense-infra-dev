b3f4dec18f582da1c3a6913313955f7ea8964d48
vot5kor <vot5kor@gmail.com>
2026-08-20 13:52:46 +0530

    expense infra dev sgs

--- sg/sg.tf
+++ sg/sg.tf
@@ -181,14 +181,14 @@ resource "aws_security_group_rule" "backend_vpn" {
 #   security_group_id = module.backend_sg.sg_id
 # }
 
-# resource "aws_security_group_rule" "mysql_backend" {
-#   type              = "ingress"
-#   from_port         = 3306
-#   to_port           = 3306
-#   protocol          = "tcp"
-#   source_security_group_id = module.backend_sg.sg_id
-#   security_group_id = module.mysql_sg.sg_id
-# }
+resource "aws_security_group_rule" "mysql_backend" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.backend_sg.sg_id
+  security_group_id = module.mysql_sg.sg_id
+}
 
 # resource "aws_security_group_rule" "web_alb_https" {
 #   type              = "ingress"
