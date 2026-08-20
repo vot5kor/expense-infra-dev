8d408cbbcb605ac5ad56510fddeab526dbbf519f
vot5kor <vot5kor@gmail.com>
2026-08-20 13:16:28 +0530

    expense infra dev

--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -40,137 +40,137 @@ resource "null_resource" "backend" {
   }
 }
 
-resource "aws_ec2_instance_state" "backend" {
-  instance_id = aws_instance.backend.id
-  state       = "stopped"
-  depends_on = [null_resource.backend]
-}
+# resource "aws_ec2_instance_state" "backend" {
+#   instance_id = aws_instance.backend.id
+#   state       = "stopped"
+#   depends_on = [null_resource.backend]
+# }
 
-resource "aws_ami_from_instance" "backend" {
-  name               = local.resource_name
-  source_instance_id = aws_instance.backend.id
-  depends_on = [aws_ec2_instance_state.backend]
-}
+# resource "aws_ami_from_instance" "backend" {
+#   name               = local.resource_name
+#   source_instance_id = aws_instance.backend.id
+#   depends_on = [aws_ec2_instance_state.backend]
+# }
 
-resource "null_resource" "backend_delete" {
+# resource "null_resource" "backend_delete" {
 
-  triggers = {
-    instance_id = aws_instance.backend.id
-  }
+#   triggers = {
+#     instance_id = aws_instance.backend.id
+#   }
 
-  provisioner "local-exec" {
-    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.backend.id}"
-  }
+#   provisioner "local-exec" {
+#     command = "aws ec2 terminate-instances --instance-ids ${aws_instance.backend.id}"
+#   }
 
-  depends_on = [aws_ami_from_instance.backend]
-}
+#   depends_on = [aws_ami_from_instance.backend]
+# }
 
-resource "aws_lb_target_group" "backend" {
-  name     = local.resource_name
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
-  deregistration_delay = 60
+# resource "aws_lb_target_group" "backend" {
+#   name     = local.resource_name
+#   port     = 8080
+#   protocol = "HTTP"
+#   vpc_id   = local.vpc_id
+#   deregistration_delay = 60
 
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 5
-    protocol = "HTTP"
-    port = 8080
-    path = "/health"
-    matcher = "200-299"
-    interval = 10
-  }
-}
+#   health_check {
+#     healthy_threshold = 2
+#     unhealthy_threshold = 2
+#     timeout = 5
+#     protocol = "HTTP"
+#     port = 8080
+#     path = "/health"
+#     matcher = "200-299"
+#     interval = 10
+#   }
+# }
 
-resource "aws_launch_template" "backend" {
-  name = local.resource_name
-  image_id = aws_ami_from_instance.backend.id
-  instance_initiated_shutdown_behavior = "terminate"
-  instance_type = "t3.micro"
-  update_default_version = true
+# resource "aws_launch_template" "backend" {
+#   name = local.resource_name
+#   image_id = aws_ami_from_instance.backend.id
+#   instance_initiated_shutdown_behavior = "terminate"
+#   instance_type = "t3.micro"
+#   update_default_version = true
 
-  vpc_security_group_ids = [local.backend_sg_id]
+#   vpc_security_group_ids = [local.backend_sg_id]
 
-  tag_specifications {
-    resource_type = "instance"
+#   tag_specifications {
+#     resource_type = "instance"
 
-    tags = {
-      Name = local.resource_name
-    }
-  }
-}
+#     tags = {
+#       Name = local.resource_name
+#     }
+#   }
+# }
 
-resource "aws_autoscaling_group" "backend" {
-  name                      = local.resource_name
-  max_size                  = 10
-  min_size                  = 1
-  health_check_grace_period = 180 # 3 minutes for instance to intialise
-  health_check_type         = "ELB"
-  desired_capacity          = 1
-  target_group_arns = [aws_lb_target_group.backend.arn]
-  launch_template {
-    id      = aws_launch_template.backend.id
-    version = "$Latest"
-  }
-  vpc_zone_identifier       = local.private_subnet_ids
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50
-    }
-    triggers = ["launch_template"]
-  }
+# resource "aws_autoscaling_group" "backend" {
+#   name                      = local.resource_name
+#   max_size                  = 10
+#   min_size                  = 1
+#   health_check_grace_period = 180 # 3 minutes for instance to intialise
+#   health_check_type         = "ELB"
+#   desired_capacity          = 1
+#   target_group_arns = [aws_lb_target_group.backend.arn]
+#   launch_template {
+#     id      = aws_launch_template.backend.id
+#     version = "$Latest"
+#   }
+#   vpc_zone_identifier       = local.private_subnet_ids
+#   instance_refresh {
+#     strategy = "Rolling"
+#     preferences {
+#       min_healthy_percentage = 50
+#     }
+#     triggers = ["launch_template"]
+#   }
 
-  tag {
-    key                 = "Name"
-    value               = local.resource_name
-    propagate_at_launch = true
-  }
+#   tag {
+#     key                 = "Name"
+#     value               = local.resource_name
+#     propagate_at_launch = true
+#   }
 
-  timeouts {
-    delete = "10m"
-  }
+#   timeouts {
+#     delete = "10m"
+#   }
 
-  tag {
-    key                 = "Project"
-    value               = "expense"
-    propagate_at_launch = false
-  }
+#   tag {
+#     key                 = "Project"
+#     value               = "expense"
+#     propagate_at_launch = false
+#   }
 
-  tag {
-    key                 = "Environment"
-    value               = "dev"
-    propagate_at_launch = false
-  }
-}
+#   tag {
+#     key                 = "Environment"
+#     value               = "dev"
+#     propagate_at_launch = false
+#   }
+# }
 
-resource "aws_autoscaling_policy" "backend" {
-  name                   = "${local.resource_name}-backend"
-  policy_type            = "TargetTrackingScaling"
-  autoscaling_group_name = aws_autoscaling_group.backend.name
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
+# resource "aws_autoscaling_policy" "backend" {
+#   name                   = "${local.resource_name}-backend"
+#   policy_type            = "TargetTrackingScaling"
+#   autoscaling_group_name = aws_autoscaling_group.backend.name
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ASGAverageCPUUtilization"
+#     }
 
-    target_value = 70.0
-  }
-}
+#     target_value = 70.0
+#   }
+# }
 
-resource "aws_lb_listener_rule" "backend" {
-  listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
-  priority     = 10
+# resource "aws_lb_listener_rule" "backend" {
+#   listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
+#   priority     = 10
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.backend.arn
-  }
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.backend.arn
+#   }
 
-  condition {
-    host_header {
-      values = ["backend.app-${var.environment}.${var.domain_name}"]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = ["backend.app-${var.environment}.${var.domain_name}"]
+#     }
+#   }
+# }
