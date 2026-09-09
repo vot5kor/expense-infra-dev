a965f4fb955f1858b63ca5408c12c614a9036314
vot5kor <vot5kor@gmail.com>
2026-09-09 17:14:03 +0530

    tf roles

--- RDS/provider.tf
+++ RDS/provider.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.84.0"
+      source = "hashicorp/aws"
+      version = "6.33.0" # Terraform AWS provider version
     }
   }
 
   backend "s3" {
-    bucket = "kvn-tf-remote-state-dev"
-    key    = "expense-dev-rds" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
-    region = "us-east-1"
-    dynamodb_table = "kvn-tf-remote-state-dev"
-    use_lockfile = true
+    bucket  = "kvn-tf-remote-state-dev" # Replace with your unique bucket name
+    key     = "roboshop-dev-db"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile   = true
+  }
 }
-}
+
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
 }
