f97d1213deb13c46f45f9827daee36667a231bb8
vot5kor <vot5kor@gmail.com>
2026-09-09 16:49:29 +0530

    tf roles

--- RDS/provider.tf
+++ RDS/provider.tf
@@ -10,7 +10,8 @@ terraform {
     bucket = "kvn-tf-remote-state-dev"
     key    = "expense-dev-rds" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
-    dynamodb_table = "kvn-tf-remote-state-dev"
+    # dynamodb_table = "kvn-tf-remote-state-dev"
+    use_lockfile = "kvn-tf-remote-state-dev"
   }
 }
 
